86579a8ecd12ab13aab07eb1ef581b2ab81943e7
Sabrina Button <sabrinarosebutton@gmail.com>
2023-09-25 12:19:10 -0400

    made modular

--- assignment1/question1.py
+++ assignment1/question1.py
@@ -1,14 +1,32 @@
+'''
+
+ELEC 326 - Group 48
+
+Assignment 1 -> Question 1 🏎️🏎️🏋️‍♂️🦀🦀
+
+Sabrina Button, Luke Major, Julia Dyck, Abby Magno
+
+'''
+
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
 
-# Part a, find PMFs
-
+# Declare the range of RVs
 x = np.arange(1,7)
 y = np.arange(3,19) # y is the RV sum of three dice rolls
 
+# Print RVs as a test
 print("RV x: ", x)
 print("RV y: ", y)
+
+# Function for quickly making plots
+def plot(name, x, y, y_label):
+    plt.figure()
+    plt.bar(x, y, align='center')
+    plt.title(name)
+    plt.ylabel(y_label)
+    plt.show()
 
 # PMF of y
 def PMF_of_y(y, num_rolls, num_sides=6):
@@ -31,38 +49,23 @@ def PMF_of_y(y, num_rolls, num_sides=6):
 
     return pmf
 
-def PMF_of_x(x):
+# PMF for y
+def PMF_of_x(x, num_sides=6):
     # Number of elements in PMF is cardinality of x
     pmf = np.zeros(x.size)
-
+    
     for i in range(x.size):
-        pmf[i] = 1/6
+        pmf[i] = 1/num_sides
 
     return pmf
 
-# plot pmf of y as bar chart
-plt.figure(1)
-plt.bar(y, PMF_of_y(y, 3), align='center')
-plt.title("PMF of RV y")
-plt.xlabel("y")
-plt.ylabel("Probability")
-plt.show()
-
-# plot pmf of x as bar graph
-plt.figure(2)
-plt.bar(x, PMF_of_x(x), align='center')
-plt.title("PMF of RV x")
-plt.xlabel("x")
-plt.ylabel("Probability")
-plt.show()
-
-# Part b, now find the CDF of each var.
-
+# PMF for y
 def CDF_of_y(y, num_rolls, num_sides=6):
     # Number of elements in CDF is cardinality of y
     cdf = np.zeros(y.size)
     denominator = num_sides**num_rolls
 
+    # iterate through possible sums and find all methods of getting that sum
     for i in range(y.size):
         # Figure out how many ways we can get y[i] with num_rolls dice
         # This is the numerator of the CDF
@@ -71,6 +74,8 @@ def CDF_of_y(y, num_rolls, num_sides=6):
             for k in range(1, num_sides+1):
                 for l in range(1, num_sides+1):
                     if j+k+l <= y[i]:
+                        # If this adds to our desired value, that means there's one more
+                        # way to get that value, so increment numerator
                         numerator += 1
 
         cdf[i] = numerator/denominator
@@ -86,25 +91,6 @@ def CDF_of_x(x):
 
     return cdf
 
-# plot cdf of y as bar chart
-plt.figure(3)
-plt.bar(y, CDF_of_y(y, 3), align='center')
-plt.title("CDF of RV y")
-plt.xlabel("y")
-plt.ylabel("Probability")
-plt.show()
-
-# plot cdf of x as bar graph
-plt.figure(4)
-plt.bar(x, CDF_of_x(x), align='center')
-plt.title("CDF of RV x")
-plt.xlabel("x")
-plt.ylabel("Probability")
-plt.show()
-
-
-
-
 def do_trials(num):
     # Try 100 dice rolls for each
     rng = np.random.default_rng()
@@ -112,6 +98,7 @@ def do_trials(num):
     x2 = rng.integers(low=1, high=7, size=(num))
     x3 = rng.integers(low=1, high=7, size=(num))
 
+    # Store sum of three rolls in y_out
     y_out = np.sum([x1, x2, x3], axis=0)
 
     # print all variables
@@ -120,6 +107,7 @@ def do_trials(num):
     print("x3: ", x3)
     print("y: ", y_out)
 
+    # Count frequency of each value of RV
     def counter(trial, outcomes=6, min=1, max=6):
         H = np.zeros(outcomes)
         for i in range(trial.size):
@@ -127,37 +115,46 @@ def do_trials(num):
 
         return H
 
+    # Count frequency of each value of RV
     H1 = counter(x1)
     H2 = counter(x2)
     H3 = counter(x3)
 
-    # plot H1, H2, H3 as bar charts
-    plt.figure(5)
+    # plot H1, H2, H3 on one bar chart
+    plt.figure()
     plt.bar(np.arange(1,7), H1, align='center')
     plt.bar(np.arange(1,7), H2, align='center')
     plt.bar(np.arange(1,7), H3, align='center')
-    plt.title("Histogram of RV H1, H2, H3")
-    plt.xlabel("x1")
+    plt.title("H1, H2, H3")
+    plt.xlabel("x")
     plt.ylabel("Frequency")
     plt.show()
 
     # Try dividing H1 by N=100
     H1 = np.divide(H1, num)
-    # plot this
-    plt.figure(6)
-    plt.bar(np.arange(1,7), H1, align='center')
-    plt.title("Histogram of RV H1/N")
-    plt.xlabel("x1")
-    plt.ylabel("Probability")
-    plt.show()
+    # plot H1/N as a bar chrt
+    plot("H1/N", x, H1, "Probability")
 
     H = counter(y_out, outcomes=16, min=3, max=18)
     # plot H as bar chart
-    plt.figure(7)
-    plt.bar(np.arange(3,19), H, align='center')
-    plt.title("Histogram of RV H")
-    plt.xlabel("y")
-    plt.ylabel("Frequency")
-    plt.show()
+    plot("H", y, H, "Frequency")
 
+# ***** Get values and figures for question 1 using above funtions: *****
+
+# I) plot pmf of y as bar chart
+plot("PMF of RV y", y, PMF_of_y(y, 3), "Probability")
+
+# I) plot pmf of x as bar chart
+plot("PMF of RV x", x, PMF_of_x(x), "Probability")
+
+# II) plot cdf of y as bar chart
+plot("CDF of RV y", y, CDF_of_y(y, 3), "Probability")
+
+# II) plot cdf of x as bar chart
+plot("CDF of RV x", x, CDF_of_x(x), "Probability")
+
+# III-V) Do 100 trials
+do_trials(100)
+
+# V) Do 1000000 trials
 do_trials(1000000)
--- assignment1/question2.py
+++ assignment1/question2.py
@@ -1,19 +1,28 @@
+'''
+
+ELEC 326 - Group 48
+
+Assignment 1 -> Question 2 🔥🔥🔥🔥💻🧠🤯
+
+Sabrina Button, Luke Major, Julia Dyck, Abby Magno
+
+'''
+
 import scipy.io
 import numpy as np
+import matplotlib.pyplot as plt
 
-# Load MATLAB file
+# Load MATLAB files
 RV1 = scipy.io.loadmat('RV1.mat')
 RV2 = scipy.io.loadmat('RV2.mat')
 RV3 = scipy.io.loadmat('RV3.mat')
 
-# Get the RVs
+# Get the RVs from MATLAB files
 RV1 = RV1['RV1']
 RV2 = RV2['RV2']
 RV3 = RV3['RV3']
 
-# Print to test
-print("RV1: ", RV1)
-
+# Count frequency of each value of RV
 def counter(trial, outcomes=100, min=0, max=100):
         H = np.zeros(outcomes)
         for i in range(trial.size):
@@ -21,34 +30,31 @@ def counter(trial, outcomes=100, min=0, max=100):
 
         return H
 
-H1 = counter(RV1[0])
-H2 = counter(RV2[0])
-H3 = counter(RV3[0])
-
-# pLOT
-import matplotlib.pyplot as plt
-plt.figure(1)
-plt.bar(np.arange(100), H1, align='center')
-plt.title("H1")
-plt.ylabel("Frequency")
-plt.show()
-
-plt.figure(2)
-plt.bar(np.arange(100), H2, align='center')
-plt.title("H2")
-plt.ylabel("Frequency")
-plt.show()
-
-plt.figure(3)
-plt.bar(np.arange(100), H3, align='center')
-plt.title("H3")
-plt.ylabel("Frequency")
-plt.show()
+# Plot bar chart of RV
+def plot(name, input, outcomes=100, min=0, max=100):
+    H = counter(input, outcomes, min, max)
+    plt.figure()
+    plt.bar(np.arange(outcomes), H, align='center')
+    plt.title(name)
+    plt.ylabel("Frequency")
+    plt.show()
 
 # Estimate probability of RV landing between 10 and 40
 def prob(H, min, max):
     return np.sum(H[min:max+1])/np.sum(H)
 
+# ***** Get values and figures for question 2 using above funtions: *****
+
+# Count frequency of each value of RV
+H1 = counter(RV1[0])
+H2 = counter(RV2[0])
+H3 = counter(RV3[0])
+
+plot("H1", H1)
 print("Probability of RV1 between 10 and 40: ", prob(H1, 10, 40))
+
+plot("H2", H2)
 print("Probability of RV2 between 10 and 40: ", prob(H2, 10, 40))
+
+plot("H3", H3)
 print("Probability of RV3 between 10 and 40: ", prob(H3, 10, 40))
